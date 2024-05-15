b39de6b4cd62d4efe3846519766d196bbbdfb82e
Crowdbotics <team@crowdbotics.com>
2024-05-15 19:18:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "slumber-v1-47967",
+  url: "https://slumber-v1-47967.botics.co",
+  api: "https://slumber-v1-47967.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
